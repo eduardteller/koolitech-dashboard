c1080470790d80775a79be55c5f32e0cee3eb7e7
Eduard <edtell@taltech.ee>
2024-06-21 02:09:58 +0300

    Fri, Jun 21, 2024  2:09:58 AM

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+	theme: {
+		extend: {
+			fontFamily: {
+				libre: ['"Libre Franklin"', 'sans-serif'],
+			},
+		},
+	},
 	content: ['./public/*.{html,js}'],
 	safelist: ['bg-red-400', 'bg-green-400'],
 	theme: {
