30abd24dcb743ef9c18fca25a65648fdabd7c27d
Kiyosaki Boss <kiyosakiboss@gmail.com>
2024-06-21 14:58:03 +0300

    Fri, Jun 21, 2024  2:58:03 PM

--- public/src/index.js
+++ public/src/index.js
@@ -1,5 +1,9 @@
 const button = document.getElementById('client-btn');
+const logo = document.getElementById('logo-btn');
 
 button.onclick = function () {
 	document.location.href = '/login';
+};
+logo.onclick = function () {
+	document.location.href = '/';
 };
